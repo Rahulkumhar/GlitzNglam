64debaa36bd1abdbfa228882279f4893c6ebfcbb
Rahulkumhar <rahul.kumar9961@gmail.com>
2024-08-23 21:15:49 +0530

    redirecttion on product list

--- src/CommonComponent/SingleCard/index.js
+++ src/CommonComponent/SingleCard/index.js
@@ -4,7 +4,7 @@ import CardMedia from "@mui/material/CardMedia";
 const SingleCardComponent = ({ item }) => {
   return (
     <CardMedia
-      sx={{ height: 400, maxWidth: 950, margin: 4, padding: 20 }}
+      sx={{ height: 400, maxWidth: 850, margin: 2 }}
       image={item.image}
       title={item.label}
     />
