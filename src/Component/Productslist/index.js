b75b05b53f20b235fc0b23724711b35ca32900ef
ManishiMehra <manishimmehra@gmail.com>
2024-08-24 19:21:06 +0530

    changes made on products page

--- src/Component/Productslist/index.js
+++ src/Component/Productslist/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Grid, Container } from "@mui/material";
 import CarouselComponent from "../../CommonComponent/CarouselComponent";
-import MultiCardComponent from "../../CommonComponent/MultiCardComponent";
+//import MultiCardComponent from "../../CommonComponent/MultiCardComponent";
 import SingleCardComponent from "../../CommonComponent/SingleCard";
-import MultipleCardsComponent from "../../CommonComponent/MultipleCardsComponent";
+//import MultipleCardsComponent from "../../CommonComponent/MultipleCardsComponent";
 import { products } from "./constant";
 import BannerImage from "../../Assets/Bangle Banner.jpg";
 import DiamondBanner from "../../Assets/Diamond Banner.jpg";
 import RingsBanner from "../../Assets/Rings Banner.jpg";
+import GoldBanner from "../../Assets/Gold Banner.jpg";
 
 const ProductsList = () => {
   return (
@@ -20,7 +21,28 @@ const ProductsList = () => {
             style={{ width: "100%", maxHeight: "400px", objectFit: "cover" }}
           />
         </div>
-        <MultiCardComponent list={products.list1} />
+        <Grid container spacing={2}>
+          {products.list1.map((item) => (
+            <Grid item xs={4} md={4}>
+              <SingleCardComponent item={item} key={item.id} />
+            </Grid>
+          ))}
+        </Grid>
+        <div style={{ textAlign: "center", margin: "20px 0" }}>
+          <img
+            src={GoldBanner}
+            alt="Banner"
+            style={{ width: "100%", maxHeight: "400px", objectFit: "cover" }}
+          />
+        </div>
+        <Grid container spacing={2}>
+          {products.list2.map((item) => (
+            <Grid item xs={4} md={4}>
+              <SingleCardComponent item={item} key={item.id} />
+            </Grid>
+          ))}
+        </Grid>
+        {/*<MultiCardComponent list={products.list1} />
 
         <Grid container spacing={2} marginBottom={2}>
           {products.list3.map((item) => (
@@ -33,7 +55,11 @@ const ProductsList = () => {
           <Grid item xs={4} md={4} marginBottom={2}>
             <MultipleCardsComponent list={products?.list2} />
           </Grid>
-        </Grid>
+        </Grid>*/}
+
+        {/*<MultipleCardsComponent list={products.list4} />*/}
+        {/*<MultiCardComponent list={products.list5} />*/}
+
         <div style={{ textAlign: "center", margin: "20px 0" }}>
           <img
             src={BannerImage}
@@ -41,19 +67,6 @@ const ProductsList = () => {
             style={{ width: "100%", maxHeight: "400px", objectFit: "cover" }}
           />
         </div>
-
-        <MultipleCardsComponent list={products.list4} />
-
-        <div style={{ textAlign: "center", margin: "20px 0" }}>
-          <img
-            src={RingsBanner}
-            alt="Banner"
-            style={{ width: "100%", maxHeight: "400px", objectFit: "cover" }}
-          />
-        </div>
-
-        <MultiCardComponent list={products.list5} />
-
         <Grid container spacing={2}>
           {products.list4.map((item) => (
             <Grid item xs={4} md={4}>
@@ -61,7 +74,20 @@ const ProductsList = () => {
             </Grid>
           ))}
         </Grid>
-
+        <div style={{ textAlign: "center", margin: "20px 0" }}>
+          <img
+            src={RingsBanner}
+            alt="Banner"
+            style={{ width: "100%", maxHeight: "400px", objectFit: "cover" }}
+          />
+        </div>
+        <Grid container spacing={2}>
+          {products.list5.map((item) => (
+            <Grid item xs={4} md={4}>
+              <SingleCardComponent item={item} key={item.id} />
+            </Grid>
+          ))}
+        </Grid>
         <CarouselComponent />
       </Container>
     </>
